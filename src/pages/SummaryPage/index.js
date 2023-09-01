24013ee9300a3027b7ee7887881af436ff9c7d69
박혜민 <hyeminpak@gmail.com>
2023-09-01 12:08:52 +0900

    summara페이지 form 구현하기

--- src/pages/SummaryPage/index.js
+++ src/pages/SummaryPage/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SummarayPage = () => {
-  return <div>SummarayPage</div>;
+  const [checked, setChecked] = useState(false);
+  return (
+    <div>
+      <form>
+        <input
+          type="checkbox"
+          id="confirm-checkbox"
+          value={checked}
+          onClick={(e) => setChecked(e.target.checked)}
+        />{" "}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button disabled={!checked} type="submit">
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SummarayPage;
